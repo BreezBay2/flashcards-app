8d113ba2a151aa21cfc73c54e52b3b3ce4239b8b
Taro Altricher <altrichter.taro@outlook.com>
2025-03-19 22:33:50 +0100

    setup authentication routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.use("/", async (req, res) => {
-    res.send("Hello World");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server up and running on ", PORT);
+    console.log("Server up and running on Port", PORT);
 });
