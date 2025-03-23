594ed0284c8e6a0fff880c05f5ccec606bc9d5f0
Taro Altricher <altrichter.taro@outlook.com>
2025-03-23 14:33:36 +0100

    get current user function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -98,7 +98,7 @@ export const login = async (req, res) => {
             email: user.email,
         });
     } catch (error) {
-        console.log("Something went wrong logging in: ", error.message);
+        console.log("Something went wrong while logging in: ", error.message);
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
@@ -109,6 +109,31 @@ export const logout = async (req, res) => {
         res.status(200).json({ message: "Logout successful" });
     } catch (error) {
         console.log("Something went wrong while logging out: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const getCurrentUser = async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: req.user.id },
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.status(200).json({
+            id: user.id,
+            fullname: user.fullname,
+            username: user.username,
+            email: user.email,
+        });
+    } catch (error) {
+        console.log(
+            "Something went wront while getting current User: ",
+            error.message
+        );
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,47 @@
+import jwt from "jsonwebtoken";
+import prisma from "../db/prisma.js";
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if (!token) {
+            return res
+                .status(401)
+                .json({ error: "Unauthorized. No Token provided." });
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decodedToken) {
+            return res
+                .status(401)
+                .json({ error: "Unauthorized. Invalid Token." });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { id: decodedToken.userId },
+            select: {
+                id: true,
+                username: true,
+                fullname: true,
+                email: true,
+            },
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found." });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(
+            "Something went wront in the Protect Route:",
+            error.message
+        );
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export default protectRoute;
